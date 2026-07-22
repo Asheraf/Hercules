b47c3f5deaed16d06d97c649c1ba451ccc3043d2
Asheraf <acheraf1998@gmail.com>
2026-07-22 09:57:23 +0100

    Implement TR_PRON_MARCH

--- src/map/skill.c
+++ src/map/skill.c
@@ -8320,6 +8320,22 @@ static int skill_castend_nodamage_id(struct block_list *src, struct block_list *
 				                          flag | BCT_PARTY | 1, skill->castend_nodamage_id);
 			}
 			break;
+		case TR_PRON_MARCH:
+			if (sd == NULL || sd->status.party_id == 0 || (flag & 1) != 0) {
+				clif->skill_nodamage(src, bl, skill_id, skill_lv,
+				                     sc_start4(src, bl, type, 100, skill_lv, 0, flag, 0,
+				                               skill->get_time(skill_id, skill_lv), skill_id));
+			} else {
+				clif->skill_nodamage(src, bl, skill_id, skill_lv, 1);
+				sd->skill_id_song = skill_id;
+				sd->skill_lv_song = skill_lv;
+				if (skill->check_pc_partner(sd, skill_id, &skill_lv, AREA_SIZE, 0) > 0)
+					flag |= 2;
+				party->foreachsamemap(skill->area_sub, sd, skill->get_splash(skill_id, skill_lv), src, skill_id,
+				                      skill_lv, tick,
+				                          flag | BCT_PARTY | 1, skill->castend_nodamage_id);
+			}
+			break;
 		case IQ_MASSIVE_F_BLASTER:
 			clif->skill_nodamage(src, bl, skill_id, skill_lv,
 			                     sc_start(src, bl, type, 100, skill_lv, skill->get_time(skill_id, skill_lv), skill_id));
--- src/map/status.c
+++ src/map/status.c
@@ -3391,6 +3391,8 @@ static void status_calc_bl_main(struct block_list *bl, e_scb_flag flag)
 			st->patk = (int32)cap_value((int64)st->patk + sc->data[SC_COMPETENTIA]->val2, 0, SHRT_MAX);
 		if (sc != NULL && sc->data[SC_ABYSS_SLAYER] != NULL)
 			st->patk = (int32)cap_value((int64)st->patk + sc->data[SC_ABYSS_SLAYER]->val2, 0, SHRT_MAX);
+		if (sc != NULL && sc->data[SC_PRON_MARCH] != NULL)
+			st->patk = (int32)cap_value((int64)st->patk + sc->data[SC_PRON_MARCH]->val2, 0, SHRT_MAX);
 		if (sc != NULL && sc->data[SC_ATTACK_STANCE] != NULL)
 			st->patk = (int32)cap_value((int64)st->patk + sc->data[SC_ATTACK_STANCE]->val3, 0, SHRT_MAX);
 	}
@@ -5953,6 +5955,8 @@ static unsigned short status_calc_speed(struct block_list *bl, struct status_cha
 			if (sc->data[SC_MOVHASTE_HORSE])
 				val = max(val, sc->data[SC_MOVHASTE_HORSE]->val1);
 			if (sc->data[SC_JAWAII_SERENADE] != NULL)
+				val = max(val, 25);
+			if (sc->data[SC_PRON_MARCH] != NULL)
 				val = max(val, 25);
 			if( sd && sd->bonus.speed_rate + sd->bonus.speed_add_rate < 0 ) // permanent item-based speedup
 				val = max( val, -(sd->bonus.speed_rate + sd->bonus.speed_add_rate) );
@@ -10400,6 +10404,11 @@ static int status_change_start_sub(struct block_list *src, struct block_list *bl
 				val2 = 3 * val1;
 				if ((val3 & 2) != 0)
 					val2 *= 2;
+			case SC_PRON_MARCH:
+				val2 = 3 * val1;
+				if ((val3 & 2) != 0)
+					val2 *= 2;
+				break;
 			case SC_GEF_NOCTURN:
 			case SC_AIN_RHAPSODY:
 				val2 = 10 * val1;
