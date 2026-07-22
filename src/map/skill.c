29def82fb15138aaa46f9f1f7b6adc3386e817ed
Asheraf <acheraf1998@gmail.com>
2026-07-22 07:02:54 +0100

    Implement BO_RESEARCHREPORT skill

--- src/map/skill.c
+++ src/map/skill.c
@@ -7428,6 +7428,10 @@ static int skill_castend_nodamage_id(struct block_list *src, struct block_list *
 				                     sc_start(src, bl, SC_PROTECTSHADOWEQUIP, 100, skill_lv,
 				                              skill->get_time(skill_id, skill_lv), skill_id));
 			break;
+		case BO_RESEARCHREPORT:
+			clif->skill_nodamage(src, bl, skill_id, skill_lv,
+			                     sc_start(src, bl, type, 100, skill_lv, skill->get_time(skill_id, skill_lv), skill_id));
+			break;
 		case WH_WIND_SIGN:
 			clif->skill_nodamage(src, bl, skill_id, skill_lv,
 			                     sc_start(src, bl, type, 100, skill_lv, skill->get_time(skill_id, skill_lv), skill_id));
