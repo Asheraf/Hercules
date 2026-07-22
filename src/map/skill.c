649506fd76a93d80e679024b9aad9e52a83c4f5d
Asheraf <acheraf1998@gmail.com>
2026-07-22 12:21:07 +0100

    Implement SOA_TALISMAN_OF_WHITE_TIGER

--- src/map/skill.c
+++ src/map/skill.c
@@ -5229,6 +5229,18 @@ static int skill_castend_damage_id(struct block_list *src, struct block_list *bl
 			skill->attack(skill->get_type(skill_id, skill_lv), src, src, bl, skill_id, skill_lv, tick, flag);
 			sc_start(src, src, SC_T_FIRST_GOD, 100, skill_lv, skill->get_time(skill_id, skill_lv), skill_id);
 			break;
+		case SOA_TALISMAN_OF_WHITE_TIGER:
+			if ((flag & 1) != 0) {
+				skill->attack(BF_MAGIC, src, src, bl, skill_id, skill_lv, tick, flag);
+			} else {
+				skill->area_temp[0] = 0;
+				skill->area_temp[1] = bl->id;
+				skill->area_temp[2] = 0;
+				map->foreachinrange(skill->area_sub, bl, skill->get_splash(skill_id, skill_lv), BL_CHAR | BL_SKILL, src,
+				                    skill_id, skill_lv, tick,
+				                    flag | BCT_ENEMY | SD_SPLASH | 1, skill->castend_damage_id);
+			}
+			break;
 		case ABC_FRENZY_SHOT:
 			clif->skill_nodamage(src, bl, skill_id, skill_lv, 1);
 			skill->attack(BF_WEAPON, src, src, bl, skill_id, skill_lv, tick, flag);
@@ -7656,6 +7668,14 @@ static int skill_castend_nodamage_id(struct block_list *src, struct block_list *
 			}
 			if (sd != NULL)
 				clif->skill_fail(sd, skill_id, USESKILL_FAIL_THIS_WEAPON, 0, 0);
+			break;
+		case SOA_TALISMAN_OF_WHITE_TIGER:
+			if (tsc != NULL && tsc->data[SC_T_FIRST_GOD] != NULL) {
+				status_change_end(src, SC_T_FIRST_GOD, INVALID_TIMER);
+				sc_start(src, src, type, 100, skill_lv, skill->get_time(skill_id, skill_lv), skill_id);
+			}
+			clif->skill_nodamage(src, bl, skill_id, skill_lv, 1);
+			skill->castend_damage_id(src, bl, skill_id, skill_lv, tick, flag);
 			break;
 		case SOA_SOUL_GATHERING:
 			clif->skill_nodamage(src, bl, skill_id, skill_lv, 1);
